86d1d8480d791008c2d89b80c275351ed3ba899a
Taewook-Jeong <jtw700@kaist.ac.kr>
2020-12-04 22:26:16 +0900

    JS Hello world! practice

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector('h1');
+/* querySelector() 'function': 'grab a reference' to heading
+'store' it in a const variable */
+myHeading.textContent = 'Hello world!';
+/*'set' myHeading's textContent 'property'*/
