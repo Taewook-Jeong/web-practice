be85975ce843fd8810c51dfd6f2a945d46508a86
Taewook-Jeong <jtw700@kaist.ac.kr>
2020-12-05 16:38:48 +0900

    finish JS basics in MDN

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,6 @@
 /*'set' myHeading's textContent 'property'*/
 
 // Image switcher code
-
 let myImage = document.querySelector('img');
 console.log('1');
 myImage.onclick = function() {
@@ -20,16 +19,28 @@ myImage.onclick = function() {
     }
 }
 
-
-/*
-let myImage = document.querySelector('img');
-
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/firefox-icon.png') {
-    myImage.setAttribute ('src','images/rose.png');
-  } else {
-    myImage.setAttribute ('src','images/firefox-icon.png');
-  }
+// Personalized message
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1'); // take references to button&h1 / store
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  // display a dialog box, asking to enter data, store
+  localStorage.setItem('name', myName);
+  // API localStorage: store data in 'browser' and retrieve it later.
+  // setItem: create&store 'item data', set value
+  myHeading.textContent = 'Mozilla is cool, ' + myName;
 }
-*/
+if(!localStorage.getItem('name')){
+  //'name' data가 존재하지 않다면
+  //없을 때 false를 return하나? -> null을 return하는데 boolean 연산에서 false 취급.
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+// 이전에 입력한 게 없다 -> setUserName으로 입력하여 문구 출력
+// 있다(이전에 입력하여 loalStorage, browser에 저장) -> 꺼내서 문구 출력
+myButton.onclick = function() {
+  setUserName();
+}
+// button에 onclick 'event handler'추가
